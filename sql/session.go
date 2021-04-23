8be53920071c59865a0a6315cf550c097090576d
Aaron Son <aaron@dolthub.com>
2021-04-23 10:53:46 -0700

    Revert "Allow SetSessionVariable to work with newer system variables"
    This reverts commit fe08b5d45759fd9c5c6c42430849b7d6a9d2171b.

--- sql/session.go
+++ sql/session.go
@@ -172,24 +172,15 @@ func (s *BaseSession) SetUserVariable(ctx *Context, varName string, value interf
 
 // GetSessionVariable implements the Session interface.
 func (s *BaseSession) GetSessionVariable(ctx *Context, sysVarName string) (interface{}, error) {
-	sysVar, _, ok := SystemVariables.GetGlobal(sysVarName)
-	if !ok {
-		return nil, ErrUnknownSystemVariable.New(sysVarName)
-	}
-	s.mu.Lock()
-	defer s.mu.Unlock()
 	val, ok := s.systemVars[strings.ToLower(sysVarName)]
 	if !ok {
-		s.systemVars[strings.ToLower(sysVarName)] = sysVar.Default
-		val = sysVar.Default
+		return nil, ErrUnknownSystemVariable.New(sysVarName)
 	}
 	return val, nil
 }
 
 // GetUserVariable implements the Session interface.
 func (s *BaseSession) GetUserVariable(ctx *Context, varName string) (Type, interface{}, error) {
-	s.mu.Lock()
-	defer s.mu.Unlock()
 	val, ok := s.userVars[strings.ToLower(varName)]
 	if !ok {
 		return Null, nil, nil
